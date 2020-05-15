7faeca0486e0a5967e2cbfabe1ebf129632c4570
Alex Cruft <alexcruft@Alexs-MacBook-Pro.local>
2020-05-14 18:05:51 -0700

    adding navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header.js';
+import Navbar from './components/Navbar.js';
 
 function App() {
   return (
