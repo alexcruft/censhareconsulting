a76b3bf553aeb6e94511a7c96fabce579654ef2c
Alex Cruft <alexcruft@Alexs-MacBook-Pro.local>
2020-05-14 18:09:47 -0700

    redo adding nav

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header.js';
-import Navbar from './components/Navbar.js';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   return (
