32c0833815dc009abd2747e9667f57e1deab64b8
Alex Cruft <alexcruft@Alexs-MacBook-Pro.local>
2020-05-14 18:01:52 -0700

    redo adding header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header.js';
+import Header from './components/Header.js';
 
 function App() {
   return (
